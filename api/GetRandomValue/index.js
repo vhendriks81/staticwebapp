212434cc0545b8a9573999a5b49890c63f4172fc
Vincent Hendriks (RS) <vincent.hendriks@teamrockstars.nl>
2020-09-16 21:09:22 +0200

    fixing function

--- api/GetRandomValue/index.js
+++ api/GetRandomValue/index.js
@@ -1,14 +1,8 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
     const crypto = require('crypto');
-    crypto.randomBytes(256, (err, buf) => {
-      if (err) throw err;
-      const responseMessage = `${buf.length} bytes of random data: ${buf.toString('hex')}`;
+    const buf = crypto.randomBytes(256);
+    const responseMessage = `var randomBytes = '${buf.toString('hex')}';`;
       context.res = {
-        // status: 200, /* Defaults to 200 */
         body: responseMessage
       };
-      context.done();
-    });
 }
